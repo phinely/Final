523e2b4531525e8c8b9b5cdc96246ed3dd827ff9
Frédéric ADJEWA <freddyadjh@gmail.com>
2021-09-06 13:25:25 +0100

    conf

--- _config.php
+++ _config.php
@@ -3,17 +3,17 @@
 ini_set('display_errors','on');
 error_reporting(E_ALL);
 
-$root = $_SERVER['DOCUMENT_ROOT'];
-$host = $_SERVER['HTTP_HOST'];
+// $root = $_SERVER['DOCUMENT_ROOT'];
+// $host = $_SERVER['HTTP_HOST'];
 
 
 define('HOST', (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http').$_SERVER['SERVER_NAME']);
 define('ROOT',$root);
 
 
-define('CONTROLLER',ROOT.'Controller/');
-define('VIEW',ROOT.'View/');
-define('MODEL',ROOT.'Model/');
-define('CLASSES',ROOT.'Classes/');
+define('CONTROLLER',ROOT.'/Controller/');
+define('VIEW',ROOT.'/View/');
+define('MODEL',ROOT.'/Model/');
+define('CLASSES',ROOT.'/Classes/');
 
-define('ASSETS',HOST.'assets/');
+define('ASSETS',HOST.'/assets/');
