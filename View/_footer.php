7d2cf830a86fd26e92180306a5753a4b23796b49
Frédéric ADJEWA <freddyadjh@gmail.com>
2021-09-06 16:01:28 +0100

    y

--- View/_footer.php
+++ View/_footer.php
@@ -31,7 +31,7 @@
 
             <div class="col-md-2 col-sm-12 allfootfa">
                 <ul>
-                    <li><a href="https://api.whatsapp.com/send/?phone=237697036836&text=dire quelque chose &app_absent=0"><i class="fab fa-whatsapp"></i> Nous joindre sur Whatsapp</a></li>
+                    <li><a href="https://api.whatsapp.com/send/?phone=237697036836&text=dites quelque chose"><i class="fab fa-whatsapp"></i> Nous joindre sur Whatsapp</a></li>
                     <li><a href="http://"><i class="fab fa-telegram-plane"></i></a> Nous joindre sur Telegram</li>
                 </ul>
             </div>
